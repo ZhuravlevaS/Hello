875685805f7f26532c41962e8d1a76a09d6ff215
NataliaPnk <nataliaa.pnnk@gmail.com>
2024-07-31 14:04:28 +0200

    show:names/balance

---
+++ natalia_panasenko/task4.js
@@ -0,0 +1,13 @@
+//Задача. Вывести имена и баланс не заблокированных юзеров не из China и не из France, если их баланс находится в диапазоне значений от 10 до 1000 (не вкл.)
+
+
+db.users.find(
+  {
+      is_blocked: { $ne: true },
+      country: { $nin: ['China', 'France'] },
+      balance: { $gt: 10, $lte: 1000 }
+  },
+  {
+      fullname: 1, balance: 1, _id: 0
+  }
+)
